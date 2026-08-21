4d081188625516a9b20e4c6f90b11bdf2f8d471f
soj <josellecallora08@gmail.com>
2026-08-22 04:29:59 +0800

    Update letter-based app and favicon icon

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "payroll-lite-ph-v1";
+const CACHE_NAME = "payroll-lite-ph-v2";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./engine.js", "./rules.js", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
